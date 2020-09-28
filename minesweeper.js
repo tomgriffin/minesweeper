fe9916d0ba4ca835d860687ace9913d1a703c6ee
Tom Griffin <t.a.griffin@gmail.com>
2020-09-28 13:40:42 +1300

    Add cell objects to cell array

--- minesweeper.js
+++ minesweeper.js
@@ -1,7 +1,19 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-var board = {}
+var board = {
+  cells: [
+    { row: 0, col: 0, isMine: false, hidden: false }, 
+    { row: 0, col: 1, isMine: false, hidden: false },
+    { row: 0, col: 2, isMine: false, hidden: false },
+    { row: 1, col: 0, isMine: true, hidden: false },
+    { row: 1, col: 1, isMine: false, hidden: false },
+    { row: 1, col: 2, isMine: false, hidden: false },
+    { row: 2, col: 0, isMine: true, hidden: false },
+    { row: 2, col: 1, isMine: false, hidden: false },
+    { row: 2, col: 2, isMine: false, hidden: false }
+  ]
+}
 
 function startGame () {
   // Don't remove this function call: it makes the game work!
